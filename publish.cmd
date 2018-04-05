9dbbfebff36033b47912cb6ba6b4d8c4f2a1e0f5
alaa-anh <36130341+alaa-anh@users.noreply.github.com>
2018-04-05 13:04:45 +0300

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=02msbot-Web-Deploy.pubxml -p:Password=D0YSGFZvc1h695jf0pf4f6GNmHTWta3w0rj1Cj9AfHjtCbJ59KwNoZLkTm9t
+
